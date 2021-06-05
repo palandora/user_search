137d24fe85a159957f3e1576f02b49ac6ff91013
palandora <bernd@dotsandlines.io>
2021-06-05 18:11:12 +0200

    rewrote createMembers function

--- script.js
+++ script.js
@@ -12,80 +12,37 @@ const members = [{name:"Melanie Moshammer", mail:"m.moshammer@gmail.com", img:"m
                 {name:"Magdalena Nahatma", mail:"m.nahatma@outlook.de", img:"magdalena_nahatma.png"},
                 {name:"Alex Baumgartner", mail:"a.baumgartner@gmail.at", img:"alex_baumgartner.png"}]
 
-function addMember(image,name,mail){
-    const cell = document.createElement("div")
-    cell.classList.add("user")
+createMembers(members)
 
-    const divider = document.createElement("div")
-    divider.classList.add("divider")
-
-    const containerInner = document.createElement("div")
-    containerInner.classList.add("bounds")
-
-    const userImage = document.createElement("img")
-    userImage.classList.add("user_image")
-    userImage.src = `assets/thumbnails/${image}`
-
-    const userName = document.createElement("span")
-    userName.classList.add("username")
-    const contentUser = document.createTextNode(`${name}`)
-    userName.appendChild(contentUser);
-
-    const userMail = document.createElement("span")
-    userMail.classList.add("usermail")
-    const contentMail = document.createTextNode(`${mail}`)
-    userMail.appendChild(contentMail)
-
-    const addUserBtn = document.createElement("img")
-    addUserBtn.src = "assets/icons/ic_next.svg"
-    addUserBtn.classList.add("add_user")
- 
-    //create inner container
-    containerInner.appendChild(userImage)
-    containerInner.appendChild(userName)
-    containerInner.appendChild(userMail)
-    containerInner.appendChild(addUserBtn)
-
-    //append inner Element to cell
-    cell.appendChild(containerInner)
-    cell.appendChild(divider)
-
-    return cell
-}
-
-function importMembers(array){
-    array.forEach(function(user) {
-        containerUser.appendChild(addMember(user.img,user.name,user.mail))
-      });
-}
-
-function removeLastMatches(){
-    const allMembers = document.querySelectorAll(".user")
-        allMembers.forEach((member)=>{
-            containerUser.removeChild(member)
-    })
+function createMembers(array){
+    const htmlString = array.map(function(obj) {
+        return `
+            <div class="user">
+                <div class="bounds">
+                    <img class="user_image" src="assets/thumbnails/${obj.img}">
+                    <span class="username">${obj.name}</span>
+                    <span class="usermail">${obj.mail}</span>
+                    <img src="assets/icons/ic_next.svg" class="add_user">
+                </div>
+                <div class="divider"></div>
+            </div>
+        `
+      }).join('')
+      containerUser.innerHTML = htmlString
 }
 
 function filterMembers(){
     textInput.addEventListener("input", (e)=>{
-        removeLastMatches()
-        const searchTerm = e.target.value;
+        const searchTerm = e.target.value.toLowerCase();
         const matches = members.filter((member)=>{
-            return member.name.indexOf(`${searchTerm}`) > -1
+            return member.name.toLowerCase().includes(searchTerm)
         })
-        importMembers(matches)
+        createMembers(matches)
         labelMembers.textContent = matches.length
     })
 }
 
-
-function selectMember(){
-
-}
-
-
-
-
-importMembers(members)
 filterMembers()
+
+
 
