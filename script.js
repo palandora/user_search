4e72082d2c51a60815a3c29c273d9e8a72cf7892
palandora <bernd@dotsandlines.io>
2021-06-05 11:23:21 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const containerUser = document.querySelector(".container_user");
+const members = [{name:"Melanie Moshammer", mail:"m.moshammer@gmail.com", img:"melanie_moshammer.png"},
+                {name:"Stefan Freisinger", mail:"stefan.freisinger@gmail.de", img:"stefan_freisinger.png"},
+                {name:"Markus Stanzer", mail:"m.stanzer@outlook.de", img:"markus_stanzer.png"},
+                {name:"Susanne Abraham", mail:"s.abraham@gmail.de", img:"susanne_abraham.png"},
+                {name:"Leo Newmann", mail:"leo.newmann@gmx.com", img:"leo_newmann.png"},
+                {name:"Magdalena Nahatma", mail:"m.nahatma@outlook.de", img:"magdalena_nahatma.png"},
+                {name:"Alex Baumgartner", mail:"a.baumgartner@gmail.at", img:"alex_baumgartner.png"}]
 
 function addMember(image,name,mail){
-    const container = document.createElement("div")
-    container.classList.add("user")
+    const cell = document.createElement("div")
+    cell.classList.add("user")
+
+    const divider = document.createElement("div")
+    divider.classList.add("divider")
+
+    const containerInner = document.createElement("div")
+    containerInner.classList.add("bounds")
 
     const userImage = document.createElement("img")
     userImage.classList.add("user_image")
-    userImage.src = `assets/thumbnails/${image}.png`
+    userImage.src = `assets/thumbnails/${image}`
 
     const userName = document.createElement("span")
     userName.classList.add("username")
@@ -17,17 +30,28 @@ function addMember(image,name,mail){
     userMail.classList.add("usermail")
     const contentMail = document.createTextNode(`${mail}`)
     userMail.appendChild(contentMail)
+
+    const addUserBtn = document.createElement("img")
+    addUserBtn.src = "assets/icons/ic_next.svg"
+    addUserBtn.classList.add("add_user")
  
+    //create inner container
+    containerInner.appendChild(userImage)
+    containerInner.appendChild(userName)
+    containerInner.appendChild(userMail)
+    containerInner.appendChild(addUserBtn)
 
+    //append inner Element to cell
+    cell.appendChild(containerInner)
+    cell.appendChild(divider)
 
-    //append all Elements to div ? chaining
-    container.appendChild(userImage)
-    container.appendChild(userName)
-    container.appendChild(userMail)
-
-    return container
+    return cell
 }
 
+function importMembers(){
+    members.forEach(function(member) {
+        containerUser.appendChild(addMember(member.img,member.name,member.mail))
+      });
+}
 
-const newMember = addMember("leo_newmann","Leo Newmann", "leo.newmann@gmail.com")
-containerUser.appendChild(newMember)
+importMembers()
